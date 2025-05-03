3ec842de3f26216c29bf960c55a16eeffe39188e
AKDesigne <webalekskubanov@gmail.com>
2025-05-03 07:59:48 +0200

    Add simple workers script without dependencies

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,50 +1,10 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
-
-/**
- * The DEBUG flag will do two things that help during development:
- * 1. we will skip caching on the edge, which makes it easier to
- *    debug.
- * 2. we will return an error message on exception in your Response rather
- *    than the default 404.html page.
- */
-const DEBUG = false
-
+// Simple static assets handler
 addEventListener('fetch', event => {
-  try {
-    event.respondWith(handleEvent(event))
-  } catch (e) {
-    if (DEBUG) {
-      return event.respondWith(
-        new Response(e.message || e.toString(), {
-          status: 500,
-        }),
-      )
-    }
-    event.respondWith(new Response('Internal Error', { status: 500 }))
-  }
+  event.respondWith(handleRequest(event.request))
 })
 
-async function handleEvent(event) {
-  const url = new URL(event.request.url)
-  let options = {}
-
-  try {
-    return await getAssetFromKV(event, options)
-  } catch (e) {
-    // if an error is thrown try to serve the asset at 404.html
-    if (!DEBUG) {
-      try {
-        let notFoundResponse = await getAssetFromKV(event, {
-          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req),
-        })
-
-        return new Response(notFoundResponse.body, {
-          ...notFoundResponse,
-          status: 404,
-        })
-      } catch (e) {}
-    }
-
-    return new Response(e.message || e.toString(), { status: 500 })
-  }
+async function handleRequest(request) {
+  const url = new URL(request.url)
+  // Serve static assets from the site bucket
+  return fetch(request)
 } 
